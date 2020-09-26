27425b73c173331acf6c92385bd1026d94563847
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2020-09-27 00:29:35 +0300

    Fix flasm parse error with specific arg names

--- src/register.js
+++ src/register.js
@@ -1,3 +1,11 @@
+const FLASM_KEYWORDS = require("./flasm-keywords.json");
+
+const flasmKeywordsLookup = new Map(FLASM_KEYWORDS.map((word) => [word, true]));
+
+function isFlasmKeyword(str) {
+  return flasmKeywordsLookup.has(str);
+}
+
 class Register {
   constructor(id, name, debugName) {
     this.id = id;
@@ -6,9 +14,14 @@ class Register {
   }
 
   toToken() {
+    // flasm fails to parse if the name in r:name is a reserved keyword
+    // seems to work if you enclose it in quotes
+    const name =
+      this.name && isFlasmKeyword(this.name) ? `'${this.name}'` : this.name;
+
     return this.debugName
-      ? `r:${this.name || this.id} /*${this.debugName}*/`
-      : `r:${this.name || this.id}`;
+      ? `r:${name || this.id} /*${this.debugName}*/`
+      : `r:${name || this.id}`;
   }
 }
 
